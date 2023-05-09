c16c50f56b4fece21dcaeb81ae52ad3d2005924d
gabstorres <127963911+gabstorres@users.noreply.github.com>
2023-05-10 03:49:52 +0800

    Update load-data.php

--- load-data.php
+++ load-data.php
@@ -2,30 +2,38 @@
 
 require "config.php";
 
-use App\Student;
+use App\Pet;
 
 try {
-	Student::register('Richard', 'Feynman', 'richard@feynman.com');
-	echo "<li>Added 1 student";
+	Pet::register('Oreo', 'Female', '2013-12-08','Ashley', 'ashleym@gmail.com', '555 Apple Street NYC', '09223344567');
+	echo "<li>Added 1 pet</li>";
 
-	$students = [
+
+	$pets = [
 		[
-			'first_name' => 'Albert',
-			'last_name' => 'Einstein',
-			'email' => 'albert@einstein.com'
+			'pet_name' => 'Krispy',
+			'pet_gender' => 'Male',
+			'pet_birthday' => '2015-11-04',
+			'owner_name' => 'Aj',
+			'email' => 'ajcastro@gmail.com',
+			'address' => '123 Corn Street NYC',
+			'contact' => '0912312322'
 		],
 		[
-			'first_name' => 'Paul',
-			'last_name' => 'Erdos',
-			'email' => 'paul@erdos.com'
+			'pet_name' => 'Basy',
+			'pet_gender' => 'Male',
+			'pet_birthday' => '2015-11-04',
+			'owner_name' => 'Dane',
+			'email' => 'danecastillo@gmail.com',
+			'address' => '223 Kiwi Street NYC',
+			'contact' => '091234567'
 		]
 	];
-	Student::registerMany($students);
-	echo "<li>Added " . count($students) . " more students";
+	Pet::registerMany($pets);
+	echo "<li>Added " . count($pets) . " more pets";
 	echo "<br /><a href='index.php'>Proceed to Index Page</a>";
 
 } catch (PDOException $e) {
 	error_log($e->getMessage());
 	echo "<h1 style='color: red'>" . $e->getMessage() . "</h1>";
 }
-
